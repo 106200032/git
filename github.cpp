4b8657ea3baef7fe0c9dddd6bc809a609de77461
DINH VAN QUANG <80462060+106200032@users.noreply.github.com>
2021-09-04 20:14:08 +0700

    Delete github.cpp

--- github.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout <<"Hello quang ";
-    cout <<"Quang dep trai!";
-    cout <<"DT-VT";
-    cout <<"Chuyen nganh KTMT";
-    return 0;
-}
